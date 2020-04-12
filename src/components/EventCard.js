e4c51641b458c29bc4b41de448f6933c0a4a9c4c
jwei98 <justin.lj.wei@gmail.com>
2020-04-12 01:54:50 -0700

    add is recurring field

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -2,42 +2,56 @@ import React from 'react';
 import '../css/eventcard.scss';
 
 class EventCard extends React.Component {
-  constructor(props) {
-    // Initialize mutable state
-    super(props);
-    const currentTime = new Date();
-    this.state = { 
-      tagsOutput: this.props.tags.join(", "),
-      isCurrent: this.props.time[0] < currentTime && currentTime < this.props.time[1],
-    };
-  }
+    constructor(props) {
+      // Initialize mutable state
+      super(props);
+      const currentTime = new Date();
+      this.state = {
+        tagsOutput: this.props.tags.join(", "),
+        isCurrent: this.props.time[0] < currentTime && currentTime < this.props.time[1],
+      };
+    }
 
-  componentDidMount() {
+    componentDidMount() {
 
-  }
+    }
 
-  render() {
-    console.log(this.props.tags)
-    return (
-      <div className="event-card">
-        <div className="card-header">{this.props.summary}</div>
-        <div className="card-subheader">{this.props.creator}</div>
-        <div className="spacer"></div>
-        <div>{this.props.description}</div>
-        <div className="spacer"></div>
-        <div className="card-subheader">TIME</div>
-        <div>{this.props.time[0].toLocaleString().replace(/(.*)\D\d+/, '$1').replace('/2020', '')} - {this.props.time[1].toLocaleString().replace(/(.*)\D\d+/, '$1').replace('/2020', '')}</div>
-        <div className="spacer"></div>
-        <div className="card-subheader">TAGS</div>
-        <div>
-          {this.state.tagsOutput}
-        </div>
-        <div className="spacer"></div>
-        {!this.state.isCurrent && <button>Add Event</button>}
-        {this.state.isCurrent && <button class="zoom-blue">Join Now</button>}
-      </div>
-    );
-  }
-}
+    render() {
+        console.log(this.props.tags)
+        return ( <
+          div className = "event-card" >
+          <
+          div className = "card-header" > {
+            this.props.summary
+          } < /div> <
+          div className = "card-subheader" > {
+            this.props.creator
+          } < /div> <
+          div className = "spacer" > < /div> <
+          div > {
+            this.props.description
+          } < /div> <
+          div className = "spacer" > < /div> <
+          div className = "card-subheader" > TIME < /div> <
+          div > {
+            this.props.time[0].toLocaleString().replace(/(.*)\D\d+/, '$1').replace('/2020', '')
+          } - {
+            this.props.time[1].toLocaleString().replace(/(.*)\D\d+/, '$1').replace('/2020', '')
+          }
+          {this.props.isRecurring && < p > Is Recurring < /p>}
+          < /div> <
+          div className = "spacer" > < /div> <
+          div className = "card-subheader" > TAGS < /div> <
+          div > {
+            this.state.tagsOutput
+          } <
+          /div> <
+          div className = "spacer" > < /div> {
+            !this.state.isCurrent && < button > Add Event < /button>} {
+              this.state.isCurrent && < button class = "zoom-blue" > Join Now < /button>} <
+                /div>
+            );
+          }
+        }
 
-export default EventCard;
+        export default EventCard;
